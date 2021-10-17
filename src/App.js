3b6ee88d93446223f9489271b9c88f95271623a0
Francisco Trejo <ftrejo@Franciscos-MacBook-Pro.local>
2021-10-17 18:04:53 -0500

    Added new footer component with styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.scss';
 import Navheader from './components/navheader/Navheader';
+import Footer from './components/footer/Footer';
 function App() {
   return (
     <div className="App">
       <Navheader></Navheader>
-      
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+      <div>
+        <footer>
+          <p>Ecommerce created during Wizeline’s Academy React Bootcamp</p>
+        </footer>
+      </div>
+    );
+}
+
+export default Footer
