5a3b0691374fdcf76ef17d10b8cb2386de75660b
JuanLuis Lopez <juanluis.lopez@wizeline.com>
2021-11-14 01:12:03 -0800

    cambios de color y fuentes en tarjetas

--- src/containers/productItem/Item.js
+++ src/containers/productItem/Item.js
@@ -3,10 +3,10 @@ function Item(props){
     const {img} = props;
     return(
         <div class="card">
-        <div class="title"><h1>{props.title}</h1></div>
+        <div class="title"><h1 class="title">{props.title}</h1></div>
            <img src={img}  />
 
-      <div class="container"><p >{props.desc}</p></div>
+      <div class="container"><h3>{props.desc}</h3></div>
     </div>
     )
 }
