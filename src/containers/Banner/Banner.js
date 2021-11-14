8e8e73666d6d996230487a7f2a247f21dd1c22e5
JuanLuis Lopez <juanluis.lopez@wizeline.com>
2021-11-14 00:27:28 -0800

    Diseño de Banner

--- src/containers/Banner/Banner.js
+++ src/containers/Banner/Banner.js
@@ -1,11 +1,12 @@
 import logo from './../../imgs/mugiwara.jpg';
-
+import "./Banner.css"
 function Banner(props){ 
     return(
-    <div>
+    <div class="header">
+        <div>
     <img src={logo} alt="logo" />
-        <h1>MugiStore!
-        </h1>
+        <h1 >MugiStore!</h1>
+        </div>
     </div>
 )
 }
