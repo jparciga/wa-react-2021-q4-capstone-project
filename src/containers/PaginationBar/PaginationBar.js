3f3b854824f50d28c2222f0943cda9ac74d4ee3f
JuanLuis Lopez <juanluis.lopez@wizeline.com>
2021-11-21 23:55:47 -0800

    Pagination Bar

---
+++ src/containers/PaginationBar/PaginationBar.js
@@ -0,0 +1,19 @@
+
+import './PaginationBar.css'
+
+const PaginationBar = () => {
+  return (
+<div class="pagination">
+  <a href="#">&laquo;</a>
+  <a href="#">1</a>
+  <a href="#">2</a>
+  <a href="#">3</a>
+  <a href="#">4</a>
+  <a href="#" class="active">5</a>
+  <a href="#">6</a>
+  <a href="#">&raquo;</a>
+</div>
+  );
+};
+
+export default PaginationBar;
--- src/containers/ProductListPage/ProductListPage.js
+++ src/containers/ProductListPage/ProductListPage.js
@@ -3,37 +3,40 @@ import Item from "../productItem/Item";
 import "./ProductListPage.css";
 import Products from "./../../mocks/en-us/products.json";
 import SideBar from "../SideBar/SideBar";
+import PaginationBar from "../PaginationBar/PaginationBar";
 
 function ProductListPage(props) {
   let items = Products.results;
-  let maxItems=0;
+  let maxItems = 0;
   return (
     <div class="float-container">
-
-
-
       <table>
         <tr>
           <td width="80%">
             {items.map((element, i) => {
-              maxItems=i;
-                return (
-                  <div class="float-child">
-                    <Item
-                      title={element.data.name}
-                      img={element.data.mainimage.url}
-                      desc={element.data.short_description}
-                      price={element.data.price}
-                      category={element.data.category.slug}
-                      width="100%"
-                    />
-                  </div>
-                );
+              maxItems = i;
+              return (
+                <div class="float-child">
+                  <Item
+                    title={element.data.name}
+                    img={element.data.mainimage.url}
+                    desc={element.data.short_description}
+                    price={element.data.price}
+                    category={element.data.category.slug}
+                    width="100%"
+                  />
+                </div>
+              );
             })}
           </td>
-        <td class="CategoriesSideBar" rowSpan={maxItems} >
-          <SideBar></SideBar>
-          </td></tr>
+          <td class="CategoriesSideBar" rowSpan={maxItems}>
+            <SideBar></SideBar>
+          </td>
+        </tr>
+        <tr>
+          <td><PaginationBar></PaginationBar></td>
+          <td></td>
+        </tr>
       </table>
     </div>
   );
